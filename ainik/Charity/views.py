3daac2ae0c6c6c1e536bb9e7ad6020843348c61e
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2023-07-24 18:42:49 +0330

    add charity create endpoint

--- ainik/Charity/views.py
+++ ainik/Charity/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import UserCharity
+from .serializers import CharitySerializer
+
+from .models import UserCharity
+
+
+class CharityCreateView(APIView):
+    def post(self, request, user_id):
+        serializer = CharitySerializer(data=request.data)
+        if serializer.is_valid():
+            charity = serializer.save()
+            user_charity = UserCharity(user_id=user_id, charity=charity)
+            user_charity.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- ainik/ainik/urls.py
+++ ainik/ainik/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from Charity.views import CharityCreateView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('users/<int:user_id>/charities/', CharityCreateView.as_view(), name='charity_create')
 ]
