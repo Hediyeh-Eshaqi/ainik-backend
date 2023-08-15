583b3a481384dc693a8096bb01d5550d769fc251
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2023-08-15 14:48:33 +0330

    get personalityComponents endpoint and some other changes

--- ainik/Accounts/views.py
+++ ainik/Accounts/views.py
@@ -3,7 +3,7 @@ from Charity.models import UserCharity, Charity
 from Charity.serializers import CharitySerializer
 from rest_framework.views import APIView
 from rest_framework import status
-from Accounts.models import PersonalityComponent
+from Accounts.models import PersonalityComponent, User
 from Accounts.serializers import PersonalityComponentsSerializer
 
 
@@ -18,15 +18,22 @@ class MyCharityView(APIView):
 class UserPersonalityComponentsView(APIView):
     def post(self, request):
         user = request.user
-        isPermitted = not PersonalityComponent.objects.filter(user = user).exists()
-        if isPermitted:
+        print(user)
+        isPermitted = PersonalityComponent.objects.all().filter(user = user).count()
+        print(isPermitted)
+        if not isPermitted:
+            request.data["user"] = user.pk
             serializer = PersonalityComponentsSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_406_NOT_ACCEPTABLE, message="this object is created before. use put to change fields")
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
     
     def get(self, request):
-        pass
+        user = request.user
+        user_pc = PersonalityComponent.objects.filter(user=user)
+        serializer = PersonalityComponentsSerializer(instance=user_pc, many= True)
+        return Response(serializer.data[0], status=status.HTTP_200_OK)
+        
         
